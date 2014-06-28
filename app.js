7690fc25037d1b5d854cad4285d7cce469dcbf76
Jared Fishman <jfish@gigbear.com>
2014-06-28 17:18:52 -0400

    established proper middleware for json support

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express                 = require("express"),
     app                     = express(),
-    server                  = require("http").createServer(app);
+    server                  = require("http").createServer(app),
+    bodyParser              = require("body-parser"),
+    http                    = require("http"),
+    fs                      = require("fs");
 
 app.set('port', 3000);
 
@@ -8,9 +11,22 @@ server.listen(3000);
 
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser());
+app.use(bodyParser.json());
 
-app.get("/", function(req, res){
+app.use(function (req, res, next){
+    console.log(req.body)
+    next()
+})
+
+app.get("/actions/new", function(req, res){
     res.render("index", {title: "hey all"});
+});
+
+
+app.post("/actions/create", function(req, res){
+    var actionName = req.body.actionName;
+    res.json({action : actionName});
 });
 
 
