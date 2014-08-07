686eede01ca4ee2bff1ebb610bec02b81f77764b
Jared Fishman <jfish@gigbear.com>
2014-08-06 20:33:03 -0700

    can access the descriptions for each of the courses, but cannot add the description to the same js object yet

--- courseScrape.js
+++ courseScrape.js
@@ -6,13 +6,20 @@ request('https://www.ccsf.edu/Schedule/Fall/computer_science.shtml', function(er
     if(!err && resp.statusCode == 200){
         var $ = cheerio.load(body);
         $('.courseTitle', 'body').each(function(){
+
+            var courseObject = {};
+
             var courseTitle = $(this).text();
+            courseObject.title = courseTitle;
+            // console.log(courseObject);
+
             var descAnchor = $(this).children("a").attr('href').toString();
             var descUrl0 = descAnchor.replace("javascript:cC(", '');
             var descUrl1 = descUrl0.replace(",\'CCS\')", '');
             var descUrl2 = descUrl1.replace("\'", '');
             var descUrl3 = descUrl2.replace("\\'", '');
             var descUrl4 = descUrl3.replace("\'", '');
+
             var sfCityCollegeURL = 'https://www.ccsf.edu/';
             var finalDescUrl = sfCityCollegeURL.concat(descUrl4);
             request(finalDescUrl, function(err, resp, body){
@@ -20,17 +27,16 @@ request('https://www.ccsf.edu/Schedule/Fall/computer_science.shtml', function(er
                     var $$ = cheerio.load(body);
                     $$('.description', 'body').each(function(){
                         var courseDescription = $$(this).text();
-                        console.log(courseDescription);
+                        courseObject.description = courseDescription;
+                        // console.log(courseObject);
+                        courses.push(courseObject);
+                        console.log(courses);
                     });
                 }
+
             });
 
-            var courseObject = {
-                title: courseTitle,
-                url: descUrl3
-            };
-            courses.push(courseObject);
         });
-        console.log(courses);
+
     }
 });
