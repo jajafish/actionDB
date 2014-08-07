17169014c9c61f7f7c2c6f329fcb16ba379aafde
Jared Fishman <jfish@gigbear.com>
2014-08-07 00:35:25 -0700

    embedded the main request function inside a larger function with variable url paramater

--- courseScrape.js
+++ courseScrape.js
@@ -1,42 +1,50 @@
 var request = require("request"),
     cheerio = require("cheerio"),
-    courses = [];
+    courses = [],
+    RSVP    = require("rsvp");
 
-request('https://www.ccsf.edu/Schedule/Fall/computer_science.shtml', function(err, resp, body){
-    if(!err && resp.statusCode == 200){
-        var $ = cheerio.load(body);
-        $('.courseTitle', 'body').each(function(){
 
-            var courseObject = {};
+var getCourseData = function(departmentUrl){
 
-            var courseTitle = $(this).text();
-            courseObject.title = courseTitle;
-            // console.log(courseObject);
+    request(departmentUrl, function(err, resp, body){
+        if(!err && resp.statusCode == 200){
+            var $ = cheerio.load(body);
+            $('.courseTitle', 'body').each(function(){
 
-            var descAnchor = $(this).children("a").attr('href').toString();
-            var descUrl0 = descAnchor.replace("javascript:cC(", '');
-            var descUrl1 = descUrl0.replace(",\'CCS\')", '');
-            var descUrl2 = descUrl1.replace("\'", '');
-            var descUrl3 = descUrl2.replace("\\'", '');
-            var descUrl4 = descUrl3.replace("\'", '');
+                var courseObject = {};
 
-            var sfCityCollegeURL = 'https://www.ccsf.edu/';
-            var finalDescUrl = sfCityCollegeURL.concat(descUrl4);
-            request(finalDescUrl, function(err, resp, body){
-                if (!err && resp.statusCode == 200){
-                    var $$ = cheerio.load(body);
-                    $$('.description', 'body').each(function(){
-                        var courseDescription = $$(this).text();
-                        courseObject.description = courseDescription;
-                        // console.log(courseObject);
-                        courses.push(courseObject);
-                        console.log(courses);
-                    });
-                }
+                var courseTitle = $(this).text();
+                courseObject.title = courseTitle;
 
+                var descAnchor = $(this).children("a").attr('href').toString();
+                var descUrl0 = descAnchor.replace("javascript:cC(", '');
+                var descUrl1 = descUrl0.replace(",\'CCS\')", '');
+                var descUrl2 = descUrl1.replace("\'", '');
+                var descUrl3 = descUrl2.replace("\\'", '');
+                var descUrl4 = descUrl3.replace("\'", '');
+
+                var sfCityCollegeURL = 'https://www.ccsf.edu/';
+                var finalDescUrl = sfCityCollegeURL.concat(descUrl4);
+                request(finalDescUrl, function(err, resp, body, print){
+                    if (!err && resp.statusCode == 200){
+                        var $$ = cheerio.load(body);
+                        $$('.description', 'body').each(function(){
+                            var courseDescription = $$(this).text();
+                            courseObject.description = courseDescription;
+                            courses.push(courseObject);
+                            console.log(courses);
+
+                        });
+                    }
+
+                });
             });
 
-        });
+        }
+    });
 
-    }
-});
+
+};
+
+
+getCourseData('https://www.ccsf.edu/Schedule/Fall/computer_science.shtml');
